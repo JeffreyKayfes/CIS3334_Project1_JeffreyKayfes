206555bc389ba797f91648ee3a3667ff59257ff5
JeffreyKayfes <jeffreykayfes@gmail.com>
2023-10-19 13:49:21 -0500

    Sprint 1 - UI Setup

---
+++ app/src/main/java/cis3334/cis3334_project1/AveragesActivity.java
@@ -0,0 +1,14 @@
+package cis3334.cis3334_project1;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class AveragesActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_averages);
+    }
+}
