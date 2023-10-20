dd05535f5d6461506081ef65122fb2d11f4e48e0
JeffreyKayfes <jeffreykayfes@gmail.com>
2023-10-19 19:19:02 -0500

    Sprint 1 - Added constraints to UI and Intent to change activities

--- app/src/main/java/cis3334/cis3334_project1/AveragesActivity.java
+++ app/src/main/java/cis3334/cis3334_project1/AveragesActivity.java
@@ -3,12 +3,37 @@ package cis3334.cis3334_project1;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
 
 public class AveragesActivity extends AppCompatActivity {
+
+    TextView textViewPointsAverage;
+    TextView textViewReboundsAverage;
+    TextView textViewAssistsAverage;
+    TextView textViewStealsAverage;
+    TextView textViewBlocksAverage;
+    TextView textViewFgAverage;
+    TextView textView3pAverage;
+    TextView textViewFtAverage;
+    Button buttonDone;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_averages);
+
+        setupDoneButton();
+    }
+
+    private void setupDoneButton() {
+        buttonDone = findViewById(R.id.buttonDone);
+        buttonDone.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                finish();
+            }
+        });
     }
 }
--- app/src/main/java/cis3334/cis3334_project1/MainActivity.java
+++ app/src/main/java/cis3334/cis3334_project1/MainActivity.java
@@ -1,14 +1,46 @@
 package cis3334.cis3334_project1;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.RecyclerView;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
 
 public class MainActivity extends AppCompatActivity {
+
+    EditText editTextPoints;
+    EditText editTextRebounds;
+    EditText editTextAssists;
+    EditText editTextSteals;
+    EditText editTextBlocks;
+    EditText editTextFgm;
+    EditText editTextFga;
+    EditText editText3pm;
+    EditText editText3pa;
+    EditText editTextFtm;
+    EditText editTextFta;
+    RecyclerView recyclerViewStats;
+    Button buttonAverages;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        setupButton();
+    }
+
+    private void setupButton() {
+        buttonAverages = findViewById(R.id.buttonAverages);
+        buttonAverages.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent secActIntent = new Intent(MainActivity.this, AveragesActivity.class);
+                startActivity(secActIntent);
+            }
+        });
     }
 }
